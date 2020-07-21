aca56ac5818a9e45d1df7be8ac38f6d55936432e
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-21 10:55:07 +0300

    Добавлен цикл ожидания успешного выполнения дампа базы данных

--- cron.daily/backupdb.sh
+++ cron.daily/backupdb.sh
@@ -4,7 +4,10 @@ mkdir /backup/$(date +%Y-%m-%d)
 cd /backup/$(date +%Y-%m-%d)
 
 echo Делаю резервное копирование базы данных
-mysqldump --host=${DB_SERVER_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} > /backup/$(date +%Y-%m-%d)/zabbix.sql
+until mysqldump --host=${DB_SERVER_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} > /backup/$(date +%Y-%m-%d)/zabbix.sql
+do
+    sleep 60
+done
 
 echo Архивирую все это добро
 
