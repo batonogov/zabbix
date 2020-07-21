4417b2f6244d8fbdf40d5e97c36048f57d80c11c
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-21 08:54:21 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,5 +1,4 @@
-docker exec zabbix-backup bash touch /var/log/etc/update-$(date +"%Y-%m-%d").log
-docker exec zabbix-backup bash /cron.daily/backupdb.sh >> /var/log/etc/update-$(date +"%Y-%m-%d").log 2>&1;
+docker exec zabbix-backup bash /etc/cron.daily/backupdb.sh
 docker pull mysql:8.0
 docker pull zabbix/zabbix-java-gateway:alpine-5.0-latest
 docker pull zabbix/zabbix-server-mysql:alpine-5.0-latest
@@ -7,4 +6,4 @@ docker pull zabbix/zabbix-web-nginx-mysql:alpine-5.0-latest
 docker-compose down
 docker-compose rm
 docker-compose up --build -d
-docker exec zabbix-backup bash restoredb.sh >> /var/log/etc/update-$(date +"%Y-%m-%d").log 2>&1;
+docker exec zabbix-backup bash restoredb.sh
