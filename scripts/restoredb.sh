ac28040c57d2cd13356aede6a64c21955b7ae665
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-22 15:42:15 +0300

    Улучшена читаемость

--- scripts/restoredb.sh
+++ scripts/restoredb.sh
@@ -1,8 +1,11 @@
 echo $(date)
+
 cd /backup
 tar -xjf `ls -t | head -1`
+
 until mysql --host=${DB_SERVER_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < *.sql
 do
     sleep 60
 done
+
 rm *.sql
