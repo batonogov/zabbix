1ae38e538ed3ae1da517f5d9d47d00e171c47111
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-07-23 09:30:20 +0300

    Дабавлен скрипт для удалени старых файлов

---
+++ cron.daily/cleaner.sh
@@ -0,0 +1,24 @@
+echo $(date)
+echo Начинаю чистку
+
+# Удаление файлов старше 7 дней в папке backup
+find /backup -type f -mtime +7 -exec rm {} \; 
+
+# Удаление файлов старше 7 дней в папке /var/log/cron
+find /var/log/cron -type f -mtime +7 -exec rm {} \; 
+
+# Удаление файлов старше 7 дней на SMB/CIFS шаре
+mkdir /temp
+
+until mount -t cifs $cifs_share /temp -o user=$login,password=$password
+do
+    sleep 60
+    echo Пробую снова
+done
+
+find /temp -type f -mtime +7 -exec rm {} \; 
+
+umount /temp
+rmdir /temp
+
+echo Чистка завершена
