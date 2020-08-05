aeac4def2eb1e80cc500c6c7e9a37b8bcb0a52d4
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-05 09:55:04 +0300

    Добавлена еще одна папка для очистки

--- cron.daily/cleaner.sh
+++ cron.daily/cleaner.sh
@@ -7,6 +7,9 @@ find /backup -type f -mtime +7 -exec rm {} \;
 # Удаление файлов старше 7 дней в папке /var/log/cron
 find /var/log/cron -type f -mtime +7 -exec rm {} \; 
 
+# Удаление файлов старше 7 дней в папке /var/log/etc
+find /var/log/etc -type f -mtime +7 -exec rm {} \; 
+
 # Удаление файлов старше 7 дней на SMB/CIFS шаре
 mkdir /temp
 
