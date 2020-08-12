4368ad3e40dfac5882806b90ffb77b42f5bbb4cd
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-12 22:13:58 +0300

    Убрано лишнее

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo $(date) >> /var/log/etc/zabbix-backup-$(date +"%Y-%m-%d").log 2>&1
-echo "Контейнер zabbix-backup запущен" >> /var/log/etc/zabbix-backup-$(date +"%Y-%m-%d").log 2>&1
 
 declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 
