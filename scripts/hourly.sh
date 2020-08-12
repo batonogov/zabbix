0cbe8f5576bb8b1d0dbd81fd505babdce178396f
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-12 22:12:38 +0300

    Добавлена дата

--- scripts/hourly.sh
+++ scripts/hourly.sh
@@ -1,6 +1,7 @@
-# Цикл перебирает все байлы в папке и выплняет их.
+# Цикл перебирает все файлы в папке и выплняет их.
 for file in /etc/cron.hourly/*
 do 
     echo "$file" >> /var/log/cron/hourly-$(date +"%Y-%m-%d").log 2>&1 
+    echo $(date) >> /var/log/cron/hourly-$(date +"%Y-%m-%d").log 2>&1;
     bash "$file" >> /var/log/cron/hourly-$(date +"%Y-%m-%d").log 2>&1
 done 
--- scripts/monthly.sh
+++ scripts/monthly.sh
@@ -1,6 +1,7 @@
-# Цикл перебирает все байлы в папке и выплняет их.
+# Цикл перебирает все файлы в папке и выплняет их.
 for file in /etc/cron.monthly/*
 do 
     echo "$file" >> /var/log/cron/monthly-$(date +"%Y-%m-%d").log 2>&1
+    echo $(date) >> /var/log/cron/monthly-$(date +"%Y-%m-%d").log 2>&1;
     bash "$file" >> /var/log/cron/monthly-$(date +"%Y-%m-%d").log 2>&1
 done
