52a2f30a3beb06308117d8cadfb8f5e2e81fcb6d
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-21 17:47:02 +0300

    Уменьшено время ожидания

--- scripts/restoredb.sh
+++ scripts/restoredb.sh
@@ -5,7 +5,7 @@ tar -xjf `ls -t | head -1`
 
 until mysql --host=${DB_SERVER_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < *.sql
 do
-    sleep 60
+    sleep 10
 done
 
 rm *.sql
