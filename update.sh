870aaa1ce6fa5c8e70c2e564c2dcd2c8f4980a72
Fedor Batonogov <52496117+batonogov@users.noreply.github.com>
2020-08-25 05:19:08 +0300

    mysql заменен на mariadb

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 docker exec zabbix-backup bash /etc/cron.daily/backupdb.sh
-docker pull mysql:8.0
+docker pull mariadb:10
 docker pull zabbix/zabbix-java-gateway:alpine-5.0-latest
 docker pull zabbix/zabbix-server-mysql:alpine-5.0-latest
 docker pull zabbix/zabbix-web-nginx-mysql:alpine-5.0-latest
