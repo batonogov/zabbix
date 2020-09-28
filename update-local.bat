0fb962b958e2daae7d5c65d1b93915b5923e9afb
Fedor Batonogov <batonogov@icloud.com>
2020-09-28 12:39:48 +0300

    Удалена лишняя строка

--- update-local.bat
+++ update-local.bat
@@ -2,7 +2,6 @@ REM Резервное копирование базы данных
 docker exec zabbix-backup bash /etc/cron.daily/backupdb.sh
 REM Остановка контенеров
 docker-compose down
-docker-compose rm
 REM Запуск контейнеров
 docker-compose --file docker-compose-local.yml up -d
 REM Восстановление базы данных
--- update-local.sh
+++ update-local.sh
@@ -2,7 +2,6 @@
 docker exec zabbix-backup bash /etc/cron.daily/backupdb.sh
 # Остановка контенеров
 docker-compose down
-docker-compose rm
 # Запуск контейнеров
 docker-compose --file docker-compose-local.yml up -d
 # Восстановление базы данных
