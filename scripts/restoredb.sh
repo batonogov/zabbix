0ad90adcb820447fcebc638cb5ba02df8f8c6bb2
Fedor Batonogov <batonogov@icloud.com>
2020-10-15 04:45:55 +0300

    Мелкие изменения

--- scripts/restoredb.sh
+++ scripts/restoredb.sh
@@ -2,8 +2,6 @@ echo $(date)
 
 echo Восстановление базы данных
 
-sleep 10
-
 until cd /backup && gunzip -c `ls -t | head -1` | mysql --host=${DB_SERVER_HOST} --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE}
 do
     sleep 10
