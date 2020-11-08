8ed57889d8fc1e771d811af6605042eee0124de7
Fedor Batonogov <batonogov@outlook.com>
2020-11-08 14:45:23 +0300

    Добавлены логи

--- update-registry.sh
+++ update-registry.sh
@@ -41,6 +41,6 @@ docker pull registry:2
 docker tag registry:2 $host/registry:2
 docker push $host/registry:2
 
-docker pull grafana/grafana:7.3.1
+docker pull grafana/grafana
 docker tag grafana/grafana $host/grafana
-docker push $host/grafana:7.3.1
+docker push $host/grafana
